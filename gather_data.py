a06d1167214f2cb8c0069e48fc83363747fda430
Raleigh Barden <rbardenksu@gmail.com>
2021-04-16 09:48:15 -0400

    add json with access keys

--- gather_data.py
+++ gather_data.py
@@ -11,10 +11,12 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 import numpy as np
+from pathlib import Path
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
+# from sklearn.metrics import mean_absolute_error
+# from sklearn.metrics import mean_squared_error
+from sklearn import metrics
 from sklearn.multioutput import MultiOutputClassifier
 import xgboost as xgb
 
@@ -71,7 +73,7 @@ def get_single_playlist(path):
     # Open path to json file, load json data
     data = json.load(open(path))
     # for i in range(len(data["playlists"])):
-    for i in range(1):
+    for i in range(len(data['playlists'])):
         # reset track_uri_arr
         track_uri_arr = []
         ith_playlist = data['playlists'][i]
@@ -91,32 +93,36 @@ def get_single_playlist(path):
         # print(new_df.keys())
         dataframe_storage.append(new_df)
     return dataframe_storage
-
+#%%
 # * Currently working on this.
-def setup_rf_model(array_of_df):
+def setup_rf_model(model, array_of_df):
     dict_x_y = get_x_y(array_of_df)
     X = dict_x_y['x']
     y = dict_x_y['y']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
-    regressor = RandomForestRegressor()
-    regressor.fit(X_train, y_train)
-    y_pred = regressor.predict(X_test)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
     print("y_pred -> ", y_pred)
-
+    mean_error = metrics.mean_absolute_error(y_test, y_pred)
+    print("\nmean_error -> ", mean_error)
+    return model
+# %%
 # * Currently working on this.
 def setup_model(array_of_df):
     dict_x_y = get_x_y(array_of_df)
-    X = dict_x_y['x']
-    y = dict_x_y['y']
+    # X = pd.Series(dict_x_y['x'])
+    # y = pd.Series(dict_x_y['y'])
+    X = pd.DataFrame(dict_x_y['x']).iloc[:,:]
+    y = pd.DataFrame(dict_x_y['y']).iloc[:,:]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
     print("x_train -> ", X_train)
     # regressor = RandomForestRegressor()
     # regressor.fit(X_train, y_train)
     # Train
     # Instantiating a XGBoost classifier object
-    X_train = pd.DataFrame(X_train)
-    y_train = pd.DataFrame(y_train)
+    # X_train = np.array(X_train)
+    # y_train = np.array(y_train)
     print("x_train", X_train)
     print("y_train", y_train)
     xgb_regressor = xgb.XGBRegressor(
@@ -151,7 +157,7 @@ def get_x_y(in_df_arr):
     out = {'x': [], 'y': []}
     # print(in_df_arr)
     for _, val in enumerate(in_df_arr):
-        print("val -> ", val)
+        #print("val -> ", val)
         split_data = split_x_y(val)
         out['x'].append(split_data['x'])
         out['y'].append(split_data['y'])
@@ -175,4 +181,11 @@ print(path)
 # %%
 array_df = get_single_playlist(path)
 # %%
-setup_model(array_df)
+#setup_model(array_df)
+
+# %%
+regressor = RandomForestRegressor()
+regressor = setup_rf_model(regressor, array_df)
+regressor = setup_rf_model(regressor, array_df)
+
+# %%
