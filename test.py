60b1c7038571be17b725a409e17203de93aaaf3e
Raleigh Barden <rbardenksu@gmail.com>
2021-04-21 11:43:38 -0400

    add dict cut method

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 # %%
-from playlists import get_features, cut_songs, spotify_conn
+from playlists import get_features, cut_songs_modified, spotify_conn
 from pathlib import Path
 import pandas as pd
 import json
 #%%
-def cut_songs_modified(tracks_array):
-    req_tracks = []
-    while len(tracks_array) > 0 and len(req_tracks) < 100:
-        req_tracks.append(tracks_array.pop())
-    return (tracks_array, req_tracks)
-
+def cut_songs_dict(tracks: dict):
+    request_tracks = dict()
+    while len(tracks) > 0 and len(request_tracks) < 100:
+        uri, features = tracks.popitem()
+        request_tracks[uri] = features
+    return (tracks, request_tracks)
 # %%
 file_dir = Path(__file__).parent
 data_dir = file_dir / 'data'
@@ -19,16 +19,16 @@ path = r'c:\Users\Raleigh\Desktop\Work\song_prediction\data\mpd.slice.0-999.json
 data = json.load(open(path))
 field_names = ["artist_name", "track_name"]
 file_tracks = []
-track_uri_arr = []
+# track_uri_arr = []
 track_uri_dict = dict()
 for ind, playlist in enumerate(data['playlists']):
     for track in playlist["tracks"]:
-        track_uri_arr.append((track["artist_name"], track["track_name"], track["track_uri"]))
+        # track_uri_arr.append((track["artist_name"], track["track_name"], track["track_uri"]))
         track_uri_dict[track["track_uri"]] = [track["artist_name"], track["track_name"]]
-print("Soung Count W/ Dupes: " + str(len(track_uri_arr)))
-track_uri_arr = list(set(track_uri_arr))
-track_uri_arr = [list(x) for x in track_uri_arr]
-print("Soung Count No Dupes: " + str(len(track_uri_arr)))
+# print("Soung Count W/ Dupes: " + str(len(track_uri_arr)))
+# track_uri_arr = list(set(track_uri_arr))
+# track_uri_arr = [list(x) for x in track_uri_arr]
+# print("Soung Count No Dupes: " + str(len(track_uri_arr)))
 # print("before loop")
 counter = 0
 while(len(track_uri_arr) > 0):
