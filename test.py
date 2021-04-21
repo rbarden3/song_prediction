39a80cdac3ce659b988f9c2845dffbfbfd2cedd0
Raleigh Barden <rbardenksu@gmail.com>
2021-04-21 11:17:29 -0400

    Basic Song set implementation

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 # %%
 from playlists import get_features, cut_songs, spotify_conn
 from pathlib import Path
+import pandas as pd
 import json
+#%%
+def cut_songs_modified(tracks_array):
+    req_tracks = []
+    while len(tracks_array) > 0 and len(req_tracks) < 100:
+        req_tracks.append(tracks_array.pop())
+    return (tracks_array, req_tracks)
+
 # %%
 file_dir = Path(__file__).parent
 data_dir = file_dir / 'data'
@@ -9,10 +17,33 @@ conn = spotify_conn(file_dir / 'keys.json')
 path = r'c:\Users\Raleigh\Desktop\Work\song_prediction\data\mpd.slice.0-999.json'
 # %%
 data = json.load(open(path))
+field_names = ["artist_name", "track_name"]
+file_tracks = []
 track_uri_arr = []
-playlist = data['playlists'][0]
-for track in playlist["tracks"]:
-    track_uri_arr.append([track["artist_name"], track["track_name"], track["track_uri"]])
+track_uri_dict = dict()
+for ind, playlist in enumerate(data['playlists']):
+    for track in playlist["tracks"]:
+        track_uri_arr.append((track["artist_name"], track["track_name"], track["track_uri"]))
+        track_uri_dict[track["track_uri"]] = [track["artist_name"], track["track_name"]]
+print("Soung Count W/ Dupes: " + str(len(track_uri_arr)))
+track_uri_arr = list(set(track_uri_arr))
+track_uri_arr = [list(x) for x in track_uri_arr]
+print("Soung Count No Dupes: " + str(len(track_uri_arr)))
+# print("before loop")
+counter = 0
+while(len(track_uri_arr) > 0):
+    counter +=1
+    # print("iter:" + str(counter))
+    track_uri_arr, request_tracks = cut_songs_modified(track_uri_arr)
+    track_uris = [track[2] for track in request_tracks]
+    # print(track_uris)
+    features_res = get_features(conn, track_uris)
+    for ind, val in enumerate(features_res):
+        del request_tracks[ind][2]
+        request_tracks[ind] += val.values()
+        request_tracks[ind] = tuple(request_tracks[ind])
+    file_tracks += request_tracks
+field_names += val.keys()
+file_tracks = set(file_tracks)
 
-track_uri_arr = cut_songs(track_uri_arr)
-features_res = get_features(conn, track_uri_arr)
+df = pd.DataFrame(data=file_tracks, columns=field_names)
