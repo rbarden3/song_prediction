fe9ef33b9b264b37a136ef43f79d6812d616d825
Raleigh Barden <rbardenksu@gmail.com>
2021-04-21 21:06:54 -0400

    get track data

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 # %%
-from playlists import get_features, cut_songs_modified, spotify_conn
+from playlists import get_features, get_all_tracks, spotify_conn
 from pathlib import Path
 import pandas as pd
+import pickle
 import json
 import time
 import requests 
@@ -17,52 +18,60 @@ def cut_songs_dict(tracks: dict):
 # %%
 file_dir = Path(__file__).parent
 data_dir = file_dir / 'data'
-conn = spotify_conn(file_dir / 'keys.json')
+pkl_path = file_dir / 'tracks_dict_noFeatures.pkl'
+conn = spotify_conn(file_dir / 'keys.json')\
+
 # path = data_dir / 'mpd.slice.0-999.json'
 # path = data_dir / 'mpd.slice.8000-8999.json'
 # %%
+all_tracks_noFeatures = dict()
 all_tracks = dict()
 time_start = time.time()
-file_times = dict()
-# file_path = data_dir / 'mpd.slice.21000-21999.json'
-for file_path in sorted(data_dir.glob('mpd.slice.*.json'), key=alphanum_key):
-# if True:
-    file_start = time.time()
-    print("file path -> ", file_path)
-    data = json.load(open(file_path))
-    files_tracks = dict()
-    for ind, playlist in enumerate(data['playlists']):
-        for track in playlist["tracks"]:
-            files_tracks[track["track_uri"]] = [track["artist_name"], track["track_name"]]
-    while(len(files_tracks) > 0):
-        files_tracks, request_tracks = cut_songs_dict(files_tracks)
-        track_uris = request_tracks.keys()
-        count = 0
-        while count < 10:
-            try:
-                features_res = get_features(conn, track_uris)
-                count = 11
-            except requests.exceptions.ReadTimeout as e:
-                print(e)
-                count +=1
-                if count < 10:
-                    print("Get Failed w/ ReadTimeout error: ", e)
-                    print("Trying Again: Attempt", count+1, "out of 10")
-                else:
-                    print("Get Failed after 10 attempts")
-                    raise(e)
-        for ind, val in enumerate(features_res):
-            try:
-                request_tracks[val['uri']] = request_tracks[val['uri']] + list(val.values())
-            except TypeError as e:
-                print("Error Assigning URI, Song Removed from Sporify:", e)
-        all_tracks.update(request_tracks)
-    file_end = time.time()
-    file_times[file_path] = file_end - file_start
+request_times = []
 
-    print("file completed in: " + str(file_times[file_path]))
+try:
+    with open(pkl_path, 'rb') as handle:
+        all_tracks_noFeatures = pickle.load(handle)
+except:
+    all_tracks_noFeatures = get_all_tracks(data_dir)
+    with open(pkl_path, 'wb') as handle:
+        pickle.dump(all_tracks, handle)
+base_length = len(all_tracks_noFeatures)
+percent_complete = int(base_length - len(all_tracks_noFeatures) / 100)
+# %%
+while(len(all_tracks_noFeatures) > 0):
+    current_percent= int(base_length - len(all_tracks_noFeatures) / 100)
+    if(percent_complete != current_percent):
+        percent_complete = current_percent
+        print(str(percent_complete) + "% Complete")
+    request_start = time.time()
+    all_tracks_noFeatures, request_tracks = cut_songs_dict(all_tracks_noFeatures)
+    track_uris = request_tracks.keys()
+    count = 0
+    while count < 10:
+        try:
+            features_res = get_features(conn, track_uris)
+            count = 11
+        except requests.exceptions.ReadTimeout as e:
+            print(e)
+            count +=1
+            if count < 10:
+                print("Get Failed w/ ReadTimeout error: ", e)
+                print("Trying Again: Attempt", count+1, "out of 10")
+            else:
+                print("Get Failed after 10 attempts")
+                raise(e)
+    for ind, val in enumerate(features_res):
+        try:
+            request_tracks[val['uri']] = request_tracks[val['uri']] + list(val.values())
+        except TypeError as e:
+            print("Error Assigning URI, Song Removed from Sporify:", e)
+    all_tracks.update(request_tracks)
+    request_end = time.time()
+    request_times.append(request_end - request_start)
+    # print("Request completed in: " + str(request_end - request_start))
 field_names = ["artist_name", "track_name"] + list(val.keys())
 df = pd.DataFrame.from_dict(data=all_tracks, orient='index', columns=field_names)
 print("results compiled in: " + str(time.time()-time_start))
-print("Average Time: " + str(sum(file_times.values())/len(file_times)))
+print("Average Time: " + str(sum(request_times)/len(request_times)))
 df.to_pickle('tracks_df.pkl')
