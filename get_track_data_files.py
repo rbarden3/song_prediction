8accc1677035c911005b093e41909dfd5933c479
Raleigh Barden <rbardenksu@gmail.com>
2021-04-22 03:02:09 -0400

    rename test.py

--- get_track_data_files.py
+++ get_track_data_files.py
@@ -1,54 +1,4 @@
 # %%
-# from playlists import get_features, cut_songs_modified, spotify_conn
-# from pathlib import Path
-# import pandas as pd
-# import json
-# %%
-
-
-# def cut_songs_dict(tracks: dict):
-#     request_tracks = dict()
-#     while len(tracks) > 0 and len(request_tracks) < 100:
-#         uri, features = tracks.popitem()
-#         request_tracks[uri] = features
-#     return (tracks, request_tracks)
-
-
-# # %%
-# file_dir = Path(__file__).parent
-# data_dir = file_dir / 'data'
-# conn = spotify_conn(file_dir / 'keys.json')
-# path = data_dir / 'mpd.slice.8000-8999.json'
-# # %%
-
-
-# def audio_features_df_knn(path, conn):
-#     data = json.load(open(path))
-#     field_names = ["artist_name", "track_name"]
-#     file_tracks = dict()
-#     track_uri_dict = dict()
-#     for ind, playlist in enumerate(data['playlists']):
-#         for track in playlist["tracks"]:
-#             track_uri_dict[track["track_uri"]] = [
-#                 track["artist_name"], track["track_name"]]
-#     counter = 0
-#     while(len(track_uri_dict) > 0):
-#         counter += 1
-#         track_uri_dict, request_tracks = cut_songs_dict(track_uri_dict)
-#         track_uris = request_tracks.keys()
-#         features_res = get_features(conn, track_uris)
-#         for ind, val in enumerate(features_res):
-#             request_tracks[val['uri']] = request_tracks[val['uri']
-#                                                         ] + list(val.values())
-#         file_tracks.update(request_tracks)
-#     field_names += val.keys()
-
-#     df = pd.DataFrame.from_dict(
-#         data=file_tracks, orient='index', columns=field_names)
-#     df = df.drop(['analysis_url', 'track_href',
-#                   'uri', 'id', 'type'], axis='columns')
-#     print(df)
-#     return df
 from playlists import get_features, get_all_tracks, spotify_conn
 from pathlib import Path
 import pandas as pd
@@ -77,8 +27,6 @@ data_dir = file_dir / 'data'
 pkl_path = file_dir / 'tracks_dict_noFeatures.pkl'
 conn = spotify_conn(file_dir / 'keys.json')\
 
-# path = data_dir / 'mpd.slice.0-999.json'
-# path = data_dir / 'mpd.slice.8000-8999.json'
 # %%
 all_tracks_noFeatures = dict()
 all_tracks = dict()
@@ -96,16 +44,8 @@ base_length = len(all_tracks_noFeatures)
 percent_complete = int(base_length - len(all_tracks_noFeatures) / base_length)
 # %%
 counter = 0
-# print("Loops Completed: " , end ="" )
 pbar = tqdm(total=int(base_length/100)+1)
 while(len(all_tracks_noFeatures) > 0):
-    # print(counter , end ="," )
-    # print(round((base_length - len(all_tracks_noFeatures)) / base_length,2) , end ="," )
-
-    # current_percent= round((base_length - len(all_tracks_noFeatures)) / base_length,2)
-    # if(percent_complete != current_percent):
-    #     percent_complete = current_percent
-    #     print(str(percent_complete) + "% Complete")
     request_start = time.time()
     all_tracks_noFeatures, request_tracks = cut_songs_dict(
         all_tracks_noFeatures)
