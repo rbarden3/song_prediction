7a1994be5b3009b4ac1578f2953232de9df8f967
Raleigh Barden <rbardenksu@gmail.com>
2021-04-22 22:47:29 -0400

    add option to iterate arrays

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 def get_col_names(dataframe_df):
     return list(dataframe_df.columns)
 
-def split_x_y(in_df, average=True):
+def split_x_y(in_df, average=True, iterate_arrays=True):
     # Col names
     # ['acousticness', 'analysis_url', 'danceability', 'duration_ms', 'energy', 'id', 'instrumentalness',
     # 'key', 'liveness', 'loudness', 'mode', 'speechiness', 'tempo', 'time_signature', 'track_href', 'type', 'uri', 'valence']
@@ -9,26 +9,34 @@ def split_x_y(in_df, average=True):
     y = []
     in_df = in_df.drop(['analysis_url', 'track_href',
                        'uri', 'id', 'type'], axis='columns')
-    if average:
+    
+    if iterate_arrays:
         while in_df.shape[0] > 1:
             in_df, last_row = in_df.drop(in_df.tail(1).index), in_df.tail(1)
-            x.append(in_df.mean().tolist())
             y.append(last_row.values.tolist()[0])
+            if average:
+                x.append(in_df.mean().tolist())
+            else:
+                x.append(in_df.values.tolist())
     else:
-        while in_df.shape[0] > 1:
-            in_df, last_row = in_df.drop(in_df.tail(1).index), in_df.tail(1)
+        in_df, last_row = in_df.drop(in_df.tail(1).index), in_df.tail(1)
+        y.append(last_row.values.tolist()[0])
+        if average:
+            x.append(in_df.mean().tolist())
+        else:
             x.append(in_df.values.tolist())
-            y.append(last_row.values.tolist()[0])
+                
+                
 
     return {'x': x, 'y': y}
 
 
-def get_x_y(in_df_arr, average=True):
+def get_x_y(in_df_arr, average=True, iterate_arrays=True):
     out = {'x': [], 'y': []}
     # print(in_df_arr)
     for _, val in enumerate(in_df_arr):
         #print("val -> ", val)
-        split_data = split_x_y(val, average)
+        split_data = split_x_y(val, average, iterate_arrays)
         for ind, _ in enumerate(split_data['x']):
             out['x'].append(split_data['x'][ind])
             out['y'].append(split_data['y'][ind])
