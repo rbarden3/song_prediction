a9a5a59796512007f8a2bef5c8f2ddcb145cdc27
amufti12 <58714536+amufti12@users.noreply.github.com>
2021-04-22 20:42:44 -0400

    Added the sequential model

--- models.py
+++ models.py
@@ -6,8 +6,18 @@ import xgboost as xgb
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Input
+
 import pandas as pd
 import numpy as np
+
+# I needed this to allow the use of my GPU
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
 def random_forest(model, array_of_df):
@@ -22,6 +32,29 @@ def random_forest(model, array_of_df):
     mean_error = mean_absolute_error(y_test, y_pred)
     print("\nmean_error -> ", mean_error)
     return model
+
+
+def sequentialModel(model, array_of_df):
+    dict_x_y = get_x_y(array_of_df)
+    X = dict_x_y['x']
+    y = dict_x_y['y']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+    input_dim = len(X_train[0])
+
+    model.add(Dense(units=13, activation='relu', input_dim=input_dim))
+    model.add(Dense(units=6, activation='relu'))
+    model.add(Dense(units=3, activation='relu'))
+    model.add(Dense(units=1, activation='sigmoid'))
+    model.compile(loss="mse", optimizer='adam', metrics=['accuracy'])
+
+    # , epochs=num_epochs, batch_size=16, verbose=0)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    print("y_pred -> ", y_pred)
+    mean_error = mean_absolute_error(y_test, y_pred)
+    print("\nmean_error -> ", mean_error)
+    return model
+
 
 def xgboost(array_of_df):
     dict_x_y = get_x_y(array_of_df)
