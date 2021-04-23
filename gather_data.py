9cd623cd23677e1625d259895c30ffdf7c59aeaa
amufti12 <58714536+amufti12@users.noreply.github.com>
2021-04-22 20:43:14 -0400

    Added imports for sequential model

--- gather_data.py
+++ gather_data.py
@@ -6,6 +6,7 @@
 # File Imports
 from sorter import alphanum_key
 from models import random_forest
+from models import sequentialModel
 from playlists import spotify_conn, get_playlists_from_file
 
 # Package Imports
@@ -14,22 +15,36 @@ import json
 import os
 from pathlib import Path
 from sklearn.ensemble import RandomForestRegressor
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+
 
 file_dir = Path(__file__).parent
 data_dir = file_dir / 'data'
 spotify = spotify_conn(file_dir / 'keys.json')
 
 # %%
-regressor = RandomForestRegressor()
+# regressor = RandomForestRegressor(warm_start=True)
+# for file_path in sorted(data_dir.glob('mpd.slice.*.json'), key=alphanum_key):
+#     print("fullpath -> ", file_path)
+# # %%
+#     array_df = get_playlists_from_file(file_path, spotify)
+
+#     # setup_model(array_df)
+
+#     regressor = random_forest(regressor, array_df)
+
+
+sequentialRegressor = tf.keras.Sequential()
 for file_path in sorted(data_dir.glob('mpd.slice.*.json'), key=alphanum_key):
     print("fullpath -> ", file_path)
     # %%
     array_df = get_playlists_from_file(file_path, spotify)
-    
+
     # setup_model(array_df)
 
-    regressor = random_forest(regressor, array_df)
-
+    sequentialRegressor = sequentialModel(sequentialRegressor, array_df)
 # %%
 data_dir = file_dir / 'data'
 filenames = os.listdir(data_dir)
