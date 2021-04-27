e49bb9146f64ccfdfe3cd1218ebe6b3502f30fa3
Raleigh Barden <rbardenksu@gmail.com>
2021-04-26 20:58:38 -0400

    clean up code

--- test_seq_model.py
+++ test_seq_model.py
@@ -17,33 +17,6 @@ import numpy as np
 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
-# print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-# sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-# def build_sequential_model():
-#     model = tf.keras.Sequential()
-#     model.add(Input(shape=(None, 13)))
-#     model.add(Reshape((42, 13))) 
-#     # model.add(Embedding(800, 13)) 
-#     model.add(LSTM(units=128, batch_input_shape=(1, None, 13), activation='relu'))
-#     model.add(Dropout(0.5))
-#     model.add(Dense(units=13, activation='relu'))
-#     model.compile(loss="mse", optimizer='adam', metrics=['accuracy', tf.keras.metrics.MeanSquaredError(), ])
-#     return model
-
-# def build_sequential_model(input_dim=376):
-#     model = tf.keras.Sequential()
-#     # model.add(LSTM(units=128, input_shape=(None, 13), activation='relu'))
-#     model.add(LSTM(units=128, input_shape=(None, 13), activation='relu'))
-
-#     model.add(Dropout(0.5))
-#     model.add(Dense(units=13, activation='relu'))
-#     model.compile(loss="mse", optimizer='adam', metrics=[
-#                   'accuracy', tf.keras.metrics.MeanSquaredError(), tf.keras.metrics.MeanAbsoluteError(), ])
-#     return model
-
-# def root_mean_squared_error(y_true, y_pred):
-#     return K.sqrt(mean_squared_error(y_true, y_pred))
-
 def root_mean_squared_error(y_true, y_pred):
     return K.sqrt(mean_squared_error(y_true, y_pred))
 
@@ -93,64 +66,7 @@ def build_sequential_model(input_dim=376):
 
 #%%
 model = build_sequential_model()
-# print("\nModel Built...\n")
 #%%
-# sequential_regressor = sequential_model(sequential_regressor, array_df)
-
-# with open('all_data.pkl', 'rb') as handle:
-#     array_df = pickle.load(handle)
-
-# print("\nData Loaded...\n")
-# #%%
-# input_dim = 376
-x = []
-y = []
-# test = False
-# if test:
-#     with open('mpd.slice.0-999.pkl', 'rb') as handle:
-#         array_df = pickle.load(handle)
-#         # print(array_df[0].columns)
-#     for df in array_df:
-#         df = df.drop(['type', 'id', 'uri', 'track_href', 'analysis_url'], axis = 1)
-#         # y.append(tf.constant(df.tail(1).values.tolist(), shape=(1, 13), dtype=np.float32))
-#         y_list = df.tail(1).values.tolist()[0]
-#         x_list = df.drop(df.tail(1).index).values.tolist()
-#         x.append(x_list)
-#         y.append(y_list)
-# else:
-#     with open('all_data.pkl', 'rb') as handle:
-#         array_df = pickle.load(handle)
-#     # for df in array_df:
-#     #     x.append(df.drop(df.tail(1).index).values.tolist())
-#     #     y.append(df.tail(1).values.tolist()[0])
-
-#     # playlists = len(array_df)
-#     playlists = 50000
-#     for _ in tqdm(range(playlists)):
-#         df = array_df.pop()
-#         df.dropna(inplace=True)
-#         if df.shape[0] > 0:
-#             y_list = df.tail(1).values.tolist()[0]
-#             x_list = df.drop(df.tail(1).index).values.tolist()
-#             y.append(y_list)
-#             x.append(x_list)
-
-#%%
-# print("\nData Formatted...\n")
-# df.tail(1).values.tolist()[0]
-# padded = pad_sequences(x)
-# X = padded
-#%%
-# from write_train_test_data import get_model_xy
-# x, y = get_model_xy(playlist_count=50000)  
-# x = pad_sequences(x)
-# y = np.asarray(y)
-
-# with open('rnn_x_dataTEST.pkl', 'wb') as handle:
-#     pickle.dump(x, handle)
-# with open('rnn_y_dataTEST.pkl', 'wb') as handle:
-#     pickle.dump(y, handle)
-
 
 with open('rnn_x_dataTEST.pkl', 'rb') as handle:
     x = pickle.load(handle)
@@ -164,9 +80,7 @@ with open('rnn_y_dataTEST.pkl', 'rb') as handle:
 #%%
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
 #%%
-# print("\nTraining Model...\n")
 model.fit(x = X_train, y = y_train, epochs=5)
-# print("\nModel Trained...\n")
 score = model.evaluate(X_test, y_test)
 print(f'Loss: {score[0]} / Accuracy: {score[1]} / MSE: {score[2]} / MAE: {score[3]}')
 # img_file = './model_arch.png'
